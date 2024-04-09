7050be3b82a2a0a8c96c31c8777a78b71b6e5354
isaac-prince <isaacsekats97@gmail.com>
2024-04-09 16:31:46 +0300

    added _layout file for stack navigation

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,11 +1,22 @@
+import { AmaticSC_700Bold } from "@expo-google-fonts/amatic-sc";
 import React from "react";
-import { Text } from "react-native";
+import { Text,StyleSheet } from "react-native";
 
 
 export default  function DayDetailsScreen(){
     return(
-        <Text>
+        <Text style={styles.text}>
                 day detail screen
         </Text>
     )
 }
+
+const styles= StyleSheet.create({
+    text:{
+         fontSize:100,
+        fontFamily: 'AmaticSC_700Bold',
+        }
+
+}
+   
+);
