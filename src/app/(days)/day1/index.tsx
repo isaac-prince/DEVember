a1681f952fe23da0ff74999b4fd6d71bb7413bc2
isaac-prince <isaacsekats97@gmail.com>
2024-04-09 15:21:16 +0300

    setup expo router

---
+++ src/app/(days)/day1/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+export default  function DayDetailsScreen(){
+    return(
+        <Text>
+                day detail screen
+        </Text>
+    )
+}
